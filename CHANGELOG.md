1b3faf57a58982f1667c532a6860d1846dfcd6e5
Yann Lojewski <yann.lojewski@id-apps.fr>
2017-06-08 22:40:37 +0200

    chore: release v1.0.0

---
+++ CHANGELOG.md
@@ -0,0 +1,11 @@
+<a name="1.0.0"></a>
+# 1.0.0 (2017-06-08)
+
+
+### Features
+
+* **arithmetic:** Implement the add operation ([001dc44](https://github.com/y4nnL/angular-arithmetic/commit/001dc44))
+* **arithmetic:** Implement the basic four operations ([725575e](https://github.com/y4nnL/angular-arithmetic/commit/725575e))
+
+
+
--- build/angular-arithmetic.js
+++ build/angular-arithmetic.js
@@ -6,8 +6,44 @@
  * @constructor
  */
 class ArithmeticService {
-  constructor() {
-    this.name = 'arithmetic';
+  /**
+   * Add n1 to n2
+   * @param {number} n1
+   * @param {number} n2
+   * @returns {number}
+   */
+  static add(n1, n2) {
+    return n1 + n2;
+  }
+
+  /**
+   * Subtract n2 from n1
+   * @param {number} n1
+   * @param {number} n2
+   * @returns {number}
+   */
+  static subtract(n1, n2) {
+    return n1 - n2;
+  }
+
+  /**
+   * Multiply n1 by n2
+   * @param {number} n1
+   * @param {number} n2
+   * @returns {number}
+   */
+  static multiply(n1, n2) {
+    return n1 * n2;
+  }
+
+  /**
+   * Divide n1 by n2
+   * @param {number} n1
+   * @param {number} n2
+   * @returns {number}
+   */
+  static divide(n1, n2) {
+    return n1 / n2;
   }
 }
 
@@ -15,4 +51,4 @@ class ArithmeticService {
 // Export
 
 angular.module('arithmetic', [])
-  .service('arithmetic', () => new ArithmeticService());
+  .service('arithmetic', () => ArithmeticService);
